12744329747ca64fdd931e56c076845d7fd82f17
Kristian Kjerstad <kristian.kjerstad@brilliant.no>
2024-06-25 09:34:27 +0200

    init projects and connect to mysql db

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # go-e-commerce-api
+
+
+# How to run run
+Start the server
+```bash
+make run
+```
---
+++ cmd/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/KristianKjerstad/go-e-commerce-api/cmd/api"
+	"github.com/KristianKjerstad/go-e-commerce-api/config"
+	"github.com/KristianKjerstad/go-e-commerce-api/db"
+	"github.com/go-sql-driver/mysql"
+)
+
+func main() {
+
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.EnvConfig.DBUser,
+		Passwd:               config.EnvConfig.DBPassword,
+		Addr:                 config.EnvConfig.DBAddress,
+		DBName:               config.EnvConfig.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	initStorage(db)
+
+	server := api.NewAPIServer(":8080", nil)
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+
+	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(("DB: connected"))
+}
